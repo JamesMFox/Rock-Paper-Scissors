ae787689158fb0f8a8d4745e9067c16861eeddaf
JamesMFox <31540421+JamesMFox@users.noreply.github.com>
2025-02-23 08:43:11 -0600

    Add function to create random commputer choice
    added a function to receive a random choice and fixed an syntax error in index.html

---
+++ javascript.js
@@ -0,0 +1,15 @@
+function getComputerChoice() {
+  let computerChoice = ""
+
+  switch (Math.floor(Math.random() * 3)) {
+    case 0:
+      return "Rock";
+      break;
+    case 1:
+      return "Paper";
+      break;
+    case 2:
+      return "Scissors";
+      break;
+  }
+}
