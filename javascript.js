ba015aa4514ff3b036104dbe35ddeaaf1b9e9cf7
JamesMFox <31540421+JamesMFox@users.noreply.github.com>
2025-02-23 09:02:55 -0600

    Add function to get human choice
    Added a function to prompt for a human choice

--- javascript.js
+++ javascript.js
@@ -1,3 +1,5 @@
+
+
 function getComputerChoice() {
   let computerChoice = ""
 
@@ -11,5 +13,28 @@ function getComputerChoice() {
     case 2:
       return "Scissors";
       break;
+  }
+}
+
+function getHumanChoice() {
+  var valid_ans = false
+  
+  while (!valid_ans) {
+    let choice = parseInt(prompt("Type 1 for Rock, 2 for Paper, or 3 for Scissors"));
+  
+    switch (choice) {
+      case 1:
+        return "Rock";
+        valid_ans = true;
+        break;
+      case 2:
+        return "Paper";
+        valid_ans = true;
+        break;
+      case 3:
+        return "Scissors";
+        valid_ans = true;
+        break;
+      }
   }
 }
